9009bf2af8441c690c9b7aab815780032bfd46a0
Per Kristian W. Engebretsen <pkengebretsen@akvagroup.com>
2022-12-07 14:07:05 +0100

    Add more pizazz

--- src/App.jsx
+++ src/App.jsx
@@ -15,17 +15,37 @@ const App = () => {
       }}
       className={css.container}
     >
-      <div className={css.planet}>
+      <motion.div
+        initial={{ scale: 0, rotate: -180 }}
+        animate={{ scale: 1, rotate: 0 }}
+        transition={{
+          type: 'spring',
+          stiffness: 100,
+          damping: 15,
+          delay: 0.5,
+        }}
+        className={css.planet}
+      >
         <Spline scene="https://prod.spline.design/ZFYn0Wdjmt628dEU/scene.splinecode" />
-      </div>
-      <div className={css.content}>
+      </motion.div>
+      <motion.div
+        initial={{ y: -500 }}
+        animate={{ y: 0 }}
+        transition={{
+          type: 'spring',
+          stiffness: 100,
+          damping: 15,
+          delay: 1.5,
+        }}
+        className={css.content}
+      >
         <h1 className={css.heading}>
           There is nothing out here yet!
         </h1>
         <h2 className={css.text}>
           But in time there will be. Stay tuned!
         </h2>
-      </div>
+      </motion.div>
     </motion.div>
   );
 };
